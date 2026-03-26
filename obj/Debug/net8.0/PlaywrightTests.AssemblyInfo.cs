809e93cccad44bb1dfb005a8c3539370e8fd5ad5
anandhang <45062262+anandhang@users.noreply.github.com>
2026-03-26 12:10:09 +0530

    test1

--- obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
+++ obj/Debug/net8.0/PlaywrightTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5c8bb3dc3c6f330436316aa80b794fb4589b900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd66cc146593962eaf8fd942dc626587925803dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
